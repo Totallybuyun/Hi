716b01dcf53dab95b6815002c2194624935023b5
Totallybuyun <long_bu_yun@s2024.ssts.edu.sg>
2024-09-09 16:11:02 +0800

    Initial Commit

---
+++ traden/tradenApp.swift
@@ -0,0 +1,17 @@
+//
+//  tradenApp.swift
+//  traden
+//
+//  Created by Long Buyun on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct tradenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
